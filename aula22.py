46bec0c1c088790bc3fe2c1a78b76be826d601cf
Cleber Coute <clbrcouto@gmail.com>
2024-04-07 21:11:26 -0300

    commit curso_pyton_02

--- aula22.py
+++ aula22.py
@@ -13,12 +13,12 @@ entrada = input('[E]ntrar [S]air: ')
 senha_digitada = input('Senha: ')
 
 senha_permitida = '123456'
-# If True:
-if entrada == 'E' and senha_digitada == senha_permitida:
+
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
     print('Entrar')
 else:
-    print('Sair')
-"""
+    print('Sair')"""
+
 # Avaliação de Curto Circuito
-print(True and False and True)
-print(True and 0 and True)
+senha = input("Senha: ") or "Sem senha"
+print(senha)
---
+++ aula23.py
@@ -0,0 +1,11 @@
+# Operador Logico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+senha = input('Senha? ')
+
+if not  senha:
+    print('Voce não digitou nada')
+
+print (not True) # False
+print (not False) # True
