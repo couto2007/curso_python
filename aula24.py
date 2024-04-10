79d56527290eab835ab112b1b4b2425c6d055b46
Cleber Couto <clbrcouto@gmail.com>
2024-04-10 17:55:35 -0300

    commit atualizando

--- aula24.py
+++ aula24.py
@@ -4,3 +4,18 @@
 # O t á v i o
 # -6-5-4-3-2-1
 
+"""nome = 'Cleber'
+print(nome[3])
+print(nome[-4])
+print('e' in nome)
+print('z' in nome)
+print(10 * ('-'))
+print('ebr' not in nome)
+"""
+nome = input('Digite o seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
---
+++ aula25.py
@@ -0,0 +1,12 @@
+"""
+Interpolação básica de strings
+S - String
+d e i - int
+f - float
+x e x - Hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'Luiz'
+preco = 1000.95897643
+variavel = ''
+print(variavel)
